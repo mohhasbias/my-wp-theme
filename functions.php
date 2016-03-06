2f43b3be10e3f81725fa16397b2a96f43135f087
Moh Hasbi Assidiqi <mohhasbias@gmail.com>
2016-03-06 14:12:05 +0700

    custom category prefix is working

--- functions.php
+++ functions.php
@@ -10,14 +10,14 @@ if(! function_exists('is_mata_kuliah')){
 		$parent_cat = get_term_by('name', 'Mata Kuliah', 'category');
 		$cat = get_the_category();
 
-		print_r($cat);
+		// print_r($cat);
 
 		return cat_is_ancestor_of($parent_cat->term_id, $cat[0]->cat_ID);
 	}
 }
 
 add_filter('get_the_archive_title', function($title){
-	echo "<h1>The $title is " . $title . "</h1>";
+	// echo "<h1>The $title is " . $title . "</h1>";
 	if(is_mata_kuliah()){
 		$title = 'Mata Kuliah: ' . single_cat_title('', false);
 	}
